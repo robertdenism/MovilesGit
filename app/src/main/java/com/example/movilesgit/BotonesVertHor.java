61d30aff9ddbc1db419745a51fbd15d912931775
robertdenism <robert1994denis@gmail.com>
2022-10-09 23:23:10 +0200

    tercera con botones

--- app/src/main/java/com/example/movilesgit/BotonesVertHor.java
+++ app/src/main/java/com/example/movilesgit/BotonesVertHor.java
@@ -9,6 +9,6 @@ public class BotonesVertHor extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_botones);
+        setContentView(R.layout.activity_botones2);
     }
 }
