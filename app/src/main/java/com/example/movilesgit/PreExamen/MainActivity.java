b0e9364edcb0e374c0968a88d430f80d770cd545
robertdenism <robert1994denis@gmail.com>
2022-11-07 17:01:28 +0100

    pppp

---
+++ app/src/main/java/com/example/movilesgit/PreExamen/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.movilesgit.PreExamen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.movilesgit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
