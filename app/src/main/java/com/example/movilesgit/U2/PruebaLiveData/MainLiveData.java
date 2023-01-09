dec868d51bde3ddc0b243c35c2048ba5987add63
robertdenism <robert1994denis@gmail.com>
2023-01-09 16:21:04 +0100

    pruebass

---
+++ app/src/main/java/com/example/movilesgit/U2/PruebaLiveData/MainLiveData.java
@@ -0,0 +1,16 @@
+package com.example.movilesgit.U2.PruebaLiveData;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.movilesgit.R;
+
+public class MainLiveData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_main_live_data);
+    }
+}
